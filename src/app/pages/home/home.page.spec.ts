59a528bfb508575b9467ea0115eb61603921369e
Nadus Dev <nadusdev@outlook.com>
2022-08-02 22:14:46 +0100

    page navigation with tdd

--- src/app/pages/home/home.page.spec.ts
+++ src/app/pages/home/home.page.spec.ts
@@ -1,24 +1,41 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 import { HomePage } from './home.page';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), AppRoutingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
+    router = TestBed.get(Router);
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to pickup-calls on see all', () => {
+    spyOn(router, 'navigate')
+    component.goToPickupCalls();
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-calls']);
+  })
+
+  it('should go to new pickup-call on see all', () => {
+    spyOn(router, 'navigate')
+    component.newPickupCall();
+    expect(router.navigate).toHaveBeenCalledWith(['pickup-call']);
+  })
+
 });
